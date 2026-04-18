e574ceabd01167d8e05ba936455b52a7a42ca453
lahteefmitco <abdul.latheef.tk@gmail.com>
2026-04-18 17:08:35 +0530

    chore: update path_provider_foundation dependency to version 2.4.4 and remove objective_c dependency from pubspec.lock

--- mobile_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import local_auth_darwin
 import package_info_plus
+import path_provider_foundation
 import shared_preferences_foundation
 import sqlite3_flutter_libs
 import url_launcher_macos
@@ -14,6 +15,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
